faca41b1d9ff7998f0f6a558281890c967925e0c
obogrelova <on.obogrelova@gmail.com>
2024-12-13 12:37:37 +0500

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+quantity = 5
+
+random_array_first = np.random.rand(quantity)
+random_array_second = np.random.rand(quantity)
+print('Первый массив')
+print(random_array_first)
+print('Второй массив')
+print(random_array_second)
+plt.figure(figsize=(10, 6))
+plt.scatter(random_array_first, random_array_second, color='blue', marker='*')
+plt.xlabel('Значения')
+plt.ylabel('Частота')
+plt.title('Диаграмма рассеяния')
+plt.grid(True)
+plt.show()
